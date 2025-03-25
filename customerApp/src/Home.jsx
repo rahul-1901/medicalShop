9d55c462f90db276c7e9a6f8b3c971dcae4cff6a
Rahul <147478589+iitian12@users.noreply.github.com>
2025-03-26 02:58:06 +0530

    smallChange

--- customerApp/src/Home.jsx
+++ customerApp/src/Home.jsx
@@ -89,7 +89,7 @@ const Home = () => {
                                             {oneUser.medicine}
                                         </td>
                                         <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>
-                                            {new Date(item.date).toLocaleDateString()}
+                                            {new Date(oneUser.date).toLocaleDateString()}
                                         </td>
                                     </tr>
                                 </tbody>
